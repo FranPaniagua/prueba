410c096c67771abcb9f8640b8640dffe3a04882d
Frrraaan <108290799+Frrraaan@users.noreply.github.com>
2022-06-27 10:15:23 -0600

    Prueba
    Hola

---
+++ pruebaspanda.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+import pandas as  pd
+
+datos = pd.read_json('https://gpax.io/api/cursor.recommendations?token=MTEwODAwOTMxOm82L0ZPeGJURVdqY2ZRZ01iVmIwMVBmU2dzOD0')
+
+st.line_chart(datos)
+
