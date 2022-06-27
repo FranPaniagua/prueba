5b8aa769a67f8c846132ff94598406a0a3395125
Frrraaan <108290799+Frrraaan@users.noreply.github.com>
2022-06-27 10:45:24 -0600

    ssvcx
    xcvxcvx

--- pruebaspanda.py
+++ pruebaspanda.py
@@ -4,5 +4,5 @@ import pandas as  pd
 
 datos = pd.read_json('https://gpax.io/api/cursor.recommendations?token=MTEwODAwOTMxOm82L0ZPeGJURVdqY2ZRZ01iVmIwMVBmU2dzOD0')
 
-st.bar_chart(datos)
+st.line_chart(datos)
 
